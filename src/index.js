15e0a6c4023b962aec471dda8406a2892632eb84
me.luc <lukasalmeida372@gmail.com>
2023-02-23 21:54:31 -0300

    feat: add env files and settings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(8080, () => {
-	console.log(`server is running...`);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+	console.log(`server is running on port ${PORT}...`);
 });
